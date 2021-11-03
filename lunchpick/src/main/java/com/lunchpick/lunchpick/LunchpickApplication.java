f8dcafd1e984e4fc38b292fcbef6833712af6cd1
ksh <dhkdghehfdl@gmail.com>
2021-11-03 22:44:51 +0900

    first commit

---
+++ lunchpick/src/main/java/com/lunchpick/lunchpick/LunchpickApplication.java
@@ -0,0 +1,13 @@
+package com.lunchpick.lunchpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchpickApplication.class, args);
+	}
+
+}
---
+++ lunchpick/src/test/java/com/lunchpick/lunchpick/LunchpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunchpick.lunchpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
