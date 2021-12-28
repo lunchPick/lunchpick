47b0e422fb6cc5ac6d2815dc11b2a3a94b331cf8
yuhwanwoo <6514687@naver.com>
2021-12-28 23:01:24 +0900

    yhw api 1개 고르기;

--- lunchpick/src/main/java/com/lunchpick/lunchpick/controller/PickController.java
+++ lunchpick/src/main/java/com/lunchpick/lunchpick/controller/PickController.java
@@ -6,6 +6,8 @@ import com.lunchpick.lunchpick.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Random;
+
 @RestController
 @RequiredArgsConstructor
 public class PickController {
@@ -15,11 +17,17 @@ public class PickController {
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     @ResponseBody
-    public KaKaoResponseDTO getRest() {
+    public String getRest() {
 
         // 로그인한 회원이 첫 번째임을 가정
        User user = userService.findOne(1L);
+        KaKaoResponseDTO list = locationService.getRestaurant(user);
 
-        return locationService.getRestaurant(user);
+        int size = list.documents.length;
+        Random rd = new Random();
+
+        int idx = rd.nextInt(size - 1);
+
+        return list.documents[idx].place_name;
     }
 }
