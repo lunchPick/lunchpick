1e65e2d0f208ed2f1477aaaef295e30b0ba52d36
yuhwanwoo <6514687@naver.com>
2022-01-21 18:08:49 +0900

    yhw 스프링 시큐리티 적용시작

--- lunchpick/src/main/java/com/lunchpick/lunchpick/controller/UserController.java
+++ lunchpick/src/main/java/com/lunchpick/lunchpick/controller/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +20,14 @@ public class UserController {
     public String CreateLoginForm(Model model) {
         model.addAttribute("loginForm", new LoginForm());
         return "users/loginForm";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model,
+                               @RequestParam(value = "error", required = false) String error,
+                               @RequestParam(value = "exception", required = false) String exception) {
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
+        return "/member/login";
     }
 }
